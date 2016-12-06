0ec40ac9a1fb75b8f178c7f5e03bfcfe5c6846db
Victor Hugo Pegoraro Pereira <vh.pegoraro@gmail.com>
2016-12-06 09:39:51 -0200

    Just playing …

--- src/pages/Navbar.vue
+++ src/pages/Navbar.vue
@@ -1,31 +1,54 @@
 <template>
 
-    <ul class="nav nav-pills">
-        <router-link 
-            to="/" 
-            tag="li" 
-            active-class="active" 
-            exact>
-            <a><i class="fa fa-home"></i> Home</a>
-        </router-link>
-        <router-link 
-            to="/users" 
-            tag="li" 
-            active-class="active">
-            <a><i class="fa fa-users"></i> Usuários</a>
-        </router-link>
-        <router-link 
-            to="/products" 
-            tag="li" 
-            active-class="active">
-            <a><i class="fa fa-cubes"></i> Produtos</a>
-        </router-link>
-        <router-link 
-            to="/login" 
-            tag="li" 
-            active-class="active">
-            <a><i class="fa fa-sign-out"></i> Sair</a>
-        </router-link>                
-    </ul>
+    <div class="clearfix">
+
+        <div class="pull-left">
+            <ul class="nav nav-pills">
+                <router-link 
+                    to="/" 
+                    tag="li" 
+                    active-class="active" 
+                    exact>
+                    <a><i class="fa fa-home"></i> Home</a>
+                </router-link>
+                <router-link 
+                    to="/users" 
+                    tag="li" 
+                    active-class="active">
+                    <a><i class="fa fa-users"></i> Usuários</a>
+                </router-link>
+                <router-link 
+                    to="/products" 
+                    tag="li" 
+                    active-class="active">
+                    <a><i class="fa fa-cubes"></i> Produtos</a>
+                </router-link>              
+            </ul>
+        </div> 
+
+        <div class="pull-right">
+            <ul class="nav nav-pills">
+                <router-link 
+                    to="/messages" 
+                    tag="li" 
+                    active-class="active">
+                    <a><i class="fa fa-envelope"></i> <span class="label label-pill label-danger"></span></a>
+                </router-link>
+                <router-link 
+                    to="/notifications" 
+                    tag="li" 
+                    active-class="active">
+                    <a><i class="fa fa-bell"></i> <span class="label label-pill label-danger"></span></a>
+                </router-link>
+                <router-link 
+                    to="/login" 
+                    tag="li" 
+                    active-class="active">
+                    <a><i class="fa fa-power-off"></i></a>
+                </router-link>                
+            </ul>
+        </div>      
+
+    </div>    
 
 </template>
